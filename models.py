92258d9b167588d74ba4b3cc074c6bb19cd71855
Maram-Khusheim <maram.khusheim@gmail.com>
2020-08-25 01:08:30 +0300

    models

---
+++ models.py
@@ -0,0 +1,50 @@
+import os
+from sqlalchemy import Integer, Column, Boolean, String, Date, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_path = os.environ['DATABASE_URL']
+#database_path = 'postgresql://mimi:m12345@localhost:5432/castingagency'
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Movies
+Have title and release year
+'''
+
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    release_date = Column(Date, nullable=False)
+
+
+'''
+Actores
+Have name, age and gender
+'''
+
+
+class Actors(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String, nullable=False)
