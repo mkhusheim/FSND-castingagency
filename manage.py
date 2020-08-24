988c1e10e867e46f98372748f79e2c4f30babc61
Maram-Khusheim <maram.khusheim@gmail.com>
2020-08-25 00:20:12 +0300

    edited

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import APP
-from . import models
+from .models import db
 
 db.init_app(APP)
 migrate = Migrate(APP, db)
